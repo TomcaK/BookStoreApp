3bceaea4d5233b81fdd75f7723a6a3d9481df1db
Tomas <tomcakopulety@seznam.cz>
2022-10-31 22:04:58 +0100

    Configuration update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Book store app
 BackEnd application which returns list of books and authors to client.
+
+## How to use
+<br>Insert http://localhost:8080/api/v1/books for list of books.<br>
+Insert http://localhost:8080/api/v1/authors for list of books.
+
--- src/main/java/cz/tomaskopulety/bookstore/Configuration.java
+++ src/main/java/cz/tomaskopulety/bookstore/Configuration.java
@@ -15,18 +15,23 @@ public class Configuration {
     @Bean
     CommandLineRunner commandLineRunner(BookRepository bookRepository, AuthorRepository authorRepository) {
         return args -> {
-            List<Author> authors = List.of(
-                    new Author("David Weber"),
-                    new Author("Frank Herbert")
-            );
-            authorRepository.saveAll(authors);
-            List<Book> books = List.of(
-                    new Book("On the Basilisk station", 399, authors.get(0)),
-                    new Book("Ashes of the victory", 599, authors.get(0)),
-                    new Book("Dune", 1099, authors.get(1)),
-                    new Book("Children of Dune", 899, authors.get(1))
-            );
-            bookRepository.saveAll(books);
+
+            if (authorRepository.count() == 0) {
+                List<Author> authors = List.of(
+                        new Author("David Weber"),
+                        new Author("Frank Herbert")
+                );
+                authorRepository.saveAll(authors);
+                List<Book> books = List.of(
+                        new Book("On the Basilisk station", 399, authors.get(0)),
+                        new Book("Ashes of the victory", 599, authors.get(0)),
+                        new Book("Dune", 1099, authors.get(1)),
+                        new Book("Children of Dune", 899, authors.get(1))
+                );
+                bookRepository.saveAll(books);
+            }
         };
     }
+
+
 }
