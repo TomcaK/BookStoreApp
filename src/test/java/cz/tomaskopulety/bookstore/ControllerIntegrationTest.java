cf8d2488c8531704765545ec390383becc9e1eb7
Tomas <tomcakopulety@seznam.cz>
2022-10-31 17:52:04 +0100

    test upgrade, flyway init improved

--- src/test/java/cz/tomaskopulety/bookstore/ControllerIntegrationTest.java
+++ src/test/java/cz/tomaskopulety/bookstore/ControllerIntegrationTest.java
@@ -1,20 +1,22 @@
 package cz.tomaskopulety.bookstore;
 
+import cz.tomaskopulety.bookstore.model.Author;
+import cz.tomaskopulety.bookstore.model.Book;
 import cz.tomaskopulety.bookstore.service.Service;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -25,48 +27,38 @@ class ControllerIntegrationTest {
     private MockMvc mockMvc;
     @MockBean
     private Service service;
-    private List<BookModel> bookModels = List.of(
-            new BookModel("On the Basilisk station", 399, "David Weber"),
-            new BookModel("Ashes of the victory", 599, "David Weber"),
-            new BookModel("Dune", 1099, "Frank Herbert"),
-            new BookModel("Children of Dune", 899, "Frank Herbert")
+    private List<Author> authors = List.of(
+            new Author("David Weber"),
+            new Author("Frank Herbert")
+    );
+
+    private List<Book> books = List.of(
+            new Book("On the Basilisk station", 399, authors.get(0)),
+            new Book("Ashes of the victory", 599, authors.get(0)),
+            new Book("Dune", 1099, authors.get(1)),
+            new Book("Children of Dune", 899, authors.get(1))
     );
 
     @Test
     void getBooksTest() throws Exception {
-//        when(service.getBooks())
-//                .thenReturn(bookModels.stream()
-//                        .map(bookModel -> new Book(bookModel.getName(), bookModel.getPrice()))
-//                        .collect(Collectors.toList()));
-//        MvcResult result = mockMvc.perform(get("/books"))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andReturn();
-//        Assertions.assertNotEquals("[]", result.getResponse().getContentAsString());
+        when(service.getBooks())
+                .thenReturn(books);
+        MvcResult result = mockMvc.perform(get("/books"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        Assertions.assertNotEquals("[]", result.getResponse().getContentAsString());
     }
 
     @Test
     void getAuthorsTest() throws Exception {
-//        when(service.getAuthors())
-//                .thenReturn(Set.of(
-//                        new Author("David Weber", bookModels),
-//                        new Author("Frank Herbert", bookModels)
-//                ));
-//        MvcResult result = mockMvc.perform(get("/authors"))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andReturn();
-//        Assertions.assertNotEquals("[]", result.getResponse().getContentAsString());
-    }
-
-    @Test
-    void getWelcomeMessageTest() throws Exception {
-        when(service.getWelcomeMessage()).thenReturn("Welcome to Book Store App.\nFor selecting all books write /books. \nFor selecting all authors write /authors.");
-        mockMvc.perform(get("http://localhost:8080"))
+        when(service.getAuthors())
+                .thenReturn(authors);
+        MvcResult result = mockMvc.perform(get("/authors"))
                 .andDo(print())
-                .andExpect(content().string(equalTo("Welcome to Book Store App.\nFor selecting all books write /books. \nFor selecting all authors write /authors.")))
-                .andExpect(status().isOk());
-
+                .andExpect(status().isOk())
+                .andReturn();
+        Assertions.assertNotEquals("[]", result.getResponse().getContentAsString());
     }
 
 }
