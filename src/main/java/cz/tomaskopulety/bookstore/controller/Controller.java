a7b7532164f189f08f790129394be75b5d689140
Tomas <tomcakopulety@seznam.cz>
2022-10-31 19:44:37 +0100

    url address change

--- src/main/java/cz/tomaskopulety/bookstore/controller/Controller.java
+++ src/main/java/cz/tomaskopulety/bookstore/controller/Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/")
+@RequestMapping(path = "/api/v1")
 public class Controller {
     private final Service service;
 
--- src/test/java/cz/tomaskopulety/bookstore/ControllerIntegrationTest.java
+++ src/test/java/cz/tomaskopulety/bookstore/ControllerIntegrationTest.java
@@ -43,7 +43,7 @@ class ControllerIntegrationTest {
     void getBooksTest() throws Exception {
         when(service.getBooks())
                 .thenReturn(books);
-        MvcResult result = mockMvc.perform(get("/books"))
+        MvcResult result = mockMvc.perform(get("/api/v1/books"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
@@ -54,7 +54,7 @@ class ControllerIntegrationTest {
     void getAuthorsTest() throws Exception {
         when(service.getAuthors())
                 .thenReturn(authors);
-        MvcResult result = mockMvc.perform(get("/authors"))
+        MvcResult result = mockMvc.perform(get("/api/v1/authors"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
