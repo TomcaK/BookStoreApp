4abe6a125ad518f896065b9ec8cfece91269c9dc
Tomas.Kopulety <tomcakopulety@seznam.cz>
2022-11-01 12:12:23 +0100

    refactor

--- src/main/java/cz/tomaskopulety/bookstore/dto/Mapper.java
+++ src/main/java/cz/tomaskopulety/bookstore/dto/Mapper.java
@@ -2,11 +2,8 @@ package cz.tomaskopulety.bookstore.dto;
 
 import cz.tomaskopulety.bookstore.model.Author;
 import cz.tomaskopulety.bookstore.model.Book;
-import cz.tomaskopulety.bookstore.service.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
